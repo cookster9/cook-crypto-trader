aa9799e53fa1f948e02835fdf00834189a41819b
cookster9 <acook999@gmail.com>
2020-07-24 10:27:41 -0500

    adding config back cause duh other stuff imports it

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Coinbase Credentials
+CB_CREDENTIALS = {
+    'PASSPHRASE': os.environ['CB_PASSPHRASE'],
+    'SECRET': os.environ['CB_SECRET'],
+    'KEY': os.environ['CB_KEY'],
+    'URL': os.environ['CB_URL']
+}
