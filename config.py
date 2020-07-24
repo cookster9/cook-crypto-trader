a25d486681adefed5fcfd0cbe842b7aab456b351
cookster9 <acook999@gmail.com>
2020-07-24 10:23:24 -0500

    trying deleting config

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-# Coinbase Credentials
-CB_CREDENTIALS = {
-    'PASSPHRASE': os.environ['CB_PASSPHRASE'],
-    'SECRET': os.environ['CB_SECRET'],
-    'KEY': os.environ['CB_KEY'],
-    'URL': os.environ['CB_URL']
-}
