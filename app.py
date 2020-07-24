3f7c6b0a0595b17865842c4fb47f65c9458d6b38
cookster9 <acook999@gmail.com>
2020-07-23 22:56:01 -0500

    deposit file

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, jsonify
+from deposit_funds import deposit_funds
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'] )
 def func():
     return jsonify({"message": "Hello World"})
+
+@app.route("/deposit", methods = ['GET'])
+def deposit_function():
+    resp = deposit_funds()
+    return jsonify(resp)
 
 if __name__ == '__main__':
     app.run(debug=True)
