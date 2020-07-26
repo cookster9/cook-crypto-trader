2a5da6f9f45690c08d053b4bbd1af551bb85cede
Andrew Cook <acook999@gmail.com>
2020-07-26 13:59:19 -0500

    entry point to check price stats /check

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from deposit_funds import deposit_funds
-
+from check_stats import check_stats
 
 app = Flask(__name__)
 
@@ -13,6 +13,11 @@ def deposit_function():
     amount = 10
     resp = deposit_funds(deposit_amount = amount)
     #resp = get_deposit_account()
+    return jsonify(resp)
+
+@app.route("/check", methods = ['GET'] )
+def check_stats_func():
+    resp = check_stats()
     return jsonify(resp)
 
 if __name__ == '__main__':
---
+++ check_stats.py
@@ -0,0 +1,39 @@
+from cbpro_client import cbpro_client
+from logger import logger
+import json
+import sys
+import string
+
+@cbpro_client
+@logger
+def check_stats(cbpro_client, logger, product = 'BTC-USD'):
+    """ Makes deposit into USD Wallet
+
+    Params: 
+        - product (pair): string default BTC-USD
+    Return: 
+        - price response
+    
+    {
+        "open": "6745.61000000", 
+        "high": "7292.11000000", 
+        "low": "6650.00000000", 
+        "volume": "26185.51325269", 
+        "last": "6813.19000000", 
+        "volume_30day": "1019451.11188405"
+    }
+
+
+    """
+    logger.info("Getting price")
+    btc_stats = cbpro_client.get_product_24hr_stats(product)
+    logger.info("Last Price: {}".format(btc_stats.get("last")))
+
+    resp = btc_stats
+
+    if 'message' in resp.keys():
+        logger.warning("message in keys?")
+    else:
+        logger.info("Stats Response: {}".format(resp))
+    
+    return resp     
